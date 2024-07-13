abbb5ea83f2e60d51f5fc4cc91e823baada5e94b
Dorian Devua <Dorian.DevUa@gmail.com>
2024-07-13 17:21:34 +0400

    add vite-tsconfig-paths and absolute imports

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'goit-react-hw-06-todo-tasks',
+  plugins: [react(), tsconfigPaths()],
+  base: 'goit-react-hw-07-todo-tasks',
   build: {
     sourcemap: true,
   },
