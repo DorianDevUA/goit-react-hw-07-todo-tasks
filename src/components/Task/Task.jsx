19b6d90c47b4579fda0b07272b5268fea1191bcf
Dorian Devua <Dorian.DevUa@gmail.com>
2024-07-14 20:58:38 +0400

    change props

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,17 +1,21 @@
 import { useDispatch } from 'react-redux';
-import { deleteTask, toggleCompleted } from 'redux/tasksSlice';
+import { deleteTask, toggleCompletedTask } from 'redux/operations';
 import { CheckBox, IconBtn, Title, Wrapper } from './Task.styled';
 
-const Task = ({ task: { id, title, completed } }) => {
+const Task = ({ task }) => {
   const dispatch = useDispatch();
 
-  const handleToggle = () => dispatch(toggleCompleted({ id }));
-  const handleDelete = () => dispatch(deleteTask({ id }));
+  const handleToggle = () => dispatch(toggleCompletedTask(task));
+  const handleDelete = () => dispatch(deleteTask(task.id));
 
   return (
     <Wrapper>
-      <CheckBox type="checkbox" checked={completed} onChange={handleToggle} />
-      <Title>{title}</Title>
+      <CheckBox
+        type="checkbox"
+        checked={task.completed}
+        onChange={handleToggle}
+      />
+      <Title>{task.title}</Title>
       <IconBtn onClick={handleDelete}>
         {/* додати іконку видалити */}
         del
